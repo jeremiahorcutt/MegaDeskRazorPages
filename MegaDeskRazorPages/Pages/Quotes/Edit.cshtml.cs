b2bbbcc00ffd45ec7c3a8e82f36f46e3ac510bba
Scott Malin <mrsemalin@gmail.com>
2021-11-05 20:29:43 -0600

    Removed Total from Edit, also allowed for case lowercase material

--- MegaDeskRazorPages/Pages/Quotes/Edit.cshtml.cs
+++ MegaDeskRazorPages/Pages/Quotes/Edit.cshtml.cs
@@ -23,6 +23,10 @@ namespace MegaDeskRazorPages.Pages.Quotes
         [BindProperty]
         public DeskQuote DeskQuote { get; set; }
 
+        [BindProperty]
+        public Desk Desk { get; set; }
+
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
@@ -47,7 +51,7 @@ namespace MegaDeskRazorPages.Pages.Quotes
             {
                 return Page();
             }
-
+            DeskQuote.QuoteTotal = GetQuote(DeskQuote.RushDays);
             _context.Attach(DeskQuote).State = EntityState.Modified;
 
             try
@@ -66,12 +70,118 @@ namespace MegaDeskRazorPages.Pages.Quotes
                 }
             }
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("./ViewAllQuotes");
         }
 
         private bool DeskQuoteExists(int id)
         {
             return _context.DeskQuote.Any(e => e.ID == id);
+        }
+        //method for calculating quote price
+        public double GetQuote(int rush)
+        {
+            Desk.area = DeskQuote.Width * DeskQuote.Depth;
+            Desk.quotePrice = DeskQuote.PRICE_BASE + CalculateAreaCost() + CalculateDrawerCost() + GetMaterialCost() + CalculateRushOrderCost(rush);
+            return Desk.quotePrice;
+        }
+
+
+        //method for calculating the area cost
+        private int CalculateAreaCost()
+        {
+            if (Desk.area > DeskQuote.LOWER_SIZE_LIMIT)
+            {
+                return (Desk.area - DeskQuote.LOWER_SIZE_LIMIT) * DeskQuote.PRICE_PER_AREA;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //method for calculating the drawer cost
+        private int CalculateDrawerCost()
+        {
+            int DrawerCost = DeskQuote.NumberOfDrawers * DeskQuote.PRICE_PER_DRAWER;
+            return DrawerCost;
+        }
+
+        //method for determining the rush costs
+        private int CalculateRushOrderCost(int rush)
+        {
+            int cost = 0;
+            switch (rush)
+            {
+                case 7:
+                    if (Desk.area < DeskQuote.LOWER_SIZE_LIMIT)
+                    {
+                        cost = 30;
+                    }
+                    else if (Desk.area > DeskQuote.UPPER_SIZE_LIMIT)
+                    {
+                        cost = 40;
+                    }
+                    else
+                    {
+                        cost = 35;
+                    }
+                    break;
+                case 5:
+                    if (Desk.area < DeskQuote.LOWER_SIZE_LIMIT)
+                    {
+                        cost = 40;
+                    }
+                    else if (Desk.area > DeskQuote.UPPER_SIZE_LIMIT)
+                    {
+                        cost = 60;
+                    }
+                    else
+                    {
+                        cost = 50;
+                    }
+                    break;
+                case 3:
+                    if (Desk.area < DeskQuote.LOWER_SIZE_LIMIT)
+                    {
+                        cost = 60;
+                    }
+                    else if (Desk.area > DeskQuote.UPPER_SIZE_LIMIT)
+                    {
+                        cost = 80;
+                    }
+                    else
+                    {
+                        cost = 70;
+                    }
+                    break;
+            }
+            return cost;
+        }
+
+        private int GetMaterialCost()
+        {
+            int cost = 0;
+            if (DeskQuote.DeskMaterial == "Pine" || DeskQuote.DeskMaterial == "pine")
+            {
+                cost = 50;
+            }
+            else if (DeskQuote.DeskMaterial == "Laminate" || DeskQuote.DeskMaterial == "laminate")
+            {
+                cost = 100;
+            }
+            else if (DeskQuote.DeskMaterial == "Veneer" || DeskQuote.DeskMaterial == "veneer")
+            {
+                cost = 125;
+            }
+            else if (DeskQuote.DeskMaterial == "Oak" || DeskQuote.DeskMaterial == "oak")
+            {
+                cost = 200;
+            }
+            else if (DeskQuote.DeskMaterial == "Rosewood" || DeskQuote.DeskMaterial == "rosewood")
+            {
+                cost = 300;
+            }
+            return cost;
         }
     }
 }
